2059426750b0f3331d6ffb57e2fe809b231e6065
Ingenierias-SAS <juantrujilloingenieriasas@gmail.com>
2025-10-20 15:01:52 -0500

    Delete cpia heder directory

--- cpia heder/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.getElementById('menu-toggle');
-  const menuNav = document.querySelector('nav ul');
-
-  menuToggle.addEventListener('click', () => {
-    menuNav.classList.toggle('active');
-  });
-});
